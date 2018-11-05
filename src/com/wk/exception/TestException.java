94fa5b3c0046f5e2c7aae2e47f0b548c6e5c9738
wangkai003 <1823569758@qq.com>
2018-11-05 14:42:12 +0800

    java中常见的异常

---
+++ src/com/wk/exception/TestException.java
@@ -0,0 +1,20 @@
+package com.wk.exception;
+
+import java.util.Scanner;
+
+/**
+ * 测试异常类的使用
+ * @author xwang
+ *
+ */
+public class TestException {
+
+	public static void main(String[] args) {
+		/*
+		 * 系统输入的类型异常
+		 * Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+        System.out.println(a);*/
+	}
+
+}
