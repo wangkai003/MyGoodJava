10360db2490f0cd09fc88e87684be0db94e9df65
wangkai003 <1823569758@qq.com>
2018-11-05 10:20:53 +0800

    文件递归循环显示

---
+++ src/com/wk/file/Testfile1.java
@@ -0,0 +1,21 @@
+package com.wk.file;
+
+import java.io.File;
+
+/**
+ * 测试file的常用方法
+ * @author xwang
+ *
+ */
+public class Testfile1 {
+     public static void main(String[] args) {
+		File f = new File("d:/java");
+		//测试是否为一个文件
+		if(f.isFile()){
+			System.out.println("是一个文件");
+		}
+		if(f.isDirectory()){
+			System.out.println("是一个目录");
+		}
+	}
+}
---
+++ src/com/wk/file/Testfile2.java
@@ -0,0 +1,32 @@
+package com.wk.file;
+
+import java.io.File;
+
+/**
+ * 递归显示指定目录和其子目录的文件
+ * @author xwang
+ *
+ */
+public class Testfile2 {
+
+	public static void main(String[] args) {
+		
+        File f = new File("D:/nodejs");
+        printFile(f, 0);
+	}
+	
+	//递归体
+	static void printFile(File file,int level){
+		for (int i = 0; i < level; i++) {
+			System.out.print("->");
+		}
+		System.out.println(file.getName());
+		if(file.isDirectory()){
+			File [] foles = file.listFiles();
+			for (File file2 : foles) {
+				printFile(file2, level+1);
+			}
+		}
+	}
+
+}
